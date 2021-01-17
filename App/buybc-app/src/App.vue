3d52d253d6c7ad4e553d52105bb7bf37b5a5d10c
bradhead <brad@zedwerks.com>
2021-01-17 09:59:02 -0800

    updated the favicon.ico and logo

--- App/buybc-app/src/App.vue
+++ App/buybc-app/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="BuyBC" src="./assets/buybc-logo">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
