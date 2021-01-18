2920bb6cf325eae715188b92cb86c6cd64f757f2
bradhead <brad@zedwerks.com>
2021-01-18 12:16:59 -0800

    put copyright at bottom of footer.

--- App/buybc-app/src/views/Footer.vue
+++ App/buybc-app/src/views/Footer.vue
@@ -2,19 +2,22 @@
   <v-footer padless absolute background-image="../assets/footer-home-01.png">
     <v-card flat tile class="lighten-1 darkgrey--text text-center">
       <v-card-text class="darkgrey--text pt-0">
-        This demonstrate how BuyBC would issue verifiable credentials for BuyBC licencees. Issuing the licenses as a VC publishes the licenses to https://dev.orgbook.gov.bc.ca
+        This demonstrate how BuyBC would issue verifiable credentials for BuyBC
+        licencees. Issuing the licenses as a VC publishes the licenses to
+        https://dev.orgbook.gov.bc.ca
       </v-card-text>
       <v-divider></v-divider>
+      <v-card-text>
+        <a href="https://www.quartech.com" target="_blank"
+          ><img src="../assets/Quartech.png" height="30"
+        /></a>
+      </v-card-text>
       <v-card-text class="darkgrey--text">
         &copy; {{ new Date().getFullYear() }} - Quartech Systems Limited
-      </v-card-text>
-            <v-card-text>
-          <a href="https://www.quartech.com" target="_blank"><img src="../assets/Quartech.png" height="30"/></a>
       </v-card-text>
     </v-card>
   </v-footer>
 </template>
 <script lang="ts">
-export default  {
-}
+export default {};
 </script>
