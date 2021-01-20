b36c7a5fd457c100c2c880bd8fd846f8bbae8086
bradhead <brad@zedwerks.com>
2021-01-19 19:26:20 -0800

    updated to have IAF look

--- App/buybc-app/src/views/Footer.vue
+++ App/buybc-app/src/views/Footer.vue
@@ -1,13 +1,36 @@
 <template>
-  <v-footer padless height="auto" tile class="my-bg">
-    <v-card flat>
+  <v-footer fluid padless height="auto" color="white" class="my-bg">
       <v-img src="../assets/footer-home-01.png" max-height="300" />
-      <v-card-text>
-        <a href="https://www.quartech.com" target="_blank"
-          ><v-img contain src="../assets/Quartech.png" max-height="30"
-        /></a>
-      </v-card-text>
-    </v-card>
+      <v-row dense no-gutters>
+        <v-col cols="6">
+          <v-card flat tile color="#7B9C3D" >
+                        <v-card-text class="white--text"><h4>FUNDING PROVIDED BY:</h4></v-card-text>
+
+            <v-card-text>
+              <v-img
+                contain
+                src="@/assets/BCGov_Funding.png"
+                max-height="45" />
+              </v-card-text></v-card>
+        </v-col>
+        <v-col cols="6">
+          <v-card padless tile flat color="#7B9C3D" width="100%">
+            <v-card-text class="white--text"><h4>DEVELOPED BY:</h4></v-card-text>
+            <v-card-text>
+              <a href="https://www.quartech.com" target="_blank"
+                ><v-img
+                  contain
+                  src="@/assets/Quartech.png"
+                  max-height="45" /></a></v-card-text
+          ></v-card> </v-col>
+          <v-col cols="12">
+          <v-card padless tile flat color="#AEBB38" width="100%">
+          <v-card-text class="white--text text-center">
+            &copy; BC Investment Agriculture Foundation. All rights reserved.
+          </v-card-text></v-card></v-col>
+          </v-row>
+
+
   </v-footer>
 </template>
 <script lang="ts">
